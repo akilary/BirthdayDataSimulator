ae49e6a9e6c21531ae3e9aa9e69944c02bccd344
akihiro <a.akilary@gmail.com>
2026-02-20 15:58:24 +0500

    Added graph generation based on the existing simulations.csv, improved docstrings for clarity and readability, and added simulation_graph.png to the plots folder

--- birthday_simulator/birthday_sim.py
+++ birthday_simulator/birthday_sim.py
@@ -2,16 +2,25 @@ import csv
 import os
 from random import choices
 
+import matplotlib.pyplot as plt
+
 
 class BirthdaySim:
-    SAVE_PATH = "data/simulations.csv"
+    SIMULATIONS_SAVE_PATH = "data/simulations.csv"
+    PLOT_SAVE_PATH = "../plots/simulation_graph.png"
 
     def __init__(self, total_simulations: int = 100000, group_size: int = 23):
+        """
+        Initializes the birthday simulation
+
+        :param total_simulations: Number of times to run the simulation
+        :param group_size: Number of people in the group
+        """
         self.total_simulations = total_simulations
         self.group_size = group_size
 
     def simulate(self) -> None:
-        """Performs a simulation"""
+        """Runs the birthday paradox simulation"""
         print("\nSimulate is starting...")
 
         duplicate_count = 0
@@ -22,12 +31,12 @@ class BirthdaySim:
 
         probability = round(duplicate_count / self.total_simulations * 100, 2)
 
-        print(f"Save to {self.SAVE_PATH}")
+        print(f"Simulations save to {self.SIMULATIONS_SAVE_PATH}")
         self._save(duplicate_count, probability)
 
     @staticmethod
     def _has_duplicate(birthdays: list) -> bool:
-        """Checks if birthdays are duplicated"""
+        """Checks if there are any duplicate birthdays in the list"""
         seen = set()
         for b in birthdays:
             if b in seen:
@@ -36,9 +45,9 @@ class BirthdaySim:
         return False
 
     def _save(self, duplicate_count: int, probability: float) -> None:
-        """Saves results to csv"""
-        file_exists = os.path.exists(self.SAVE_PATH)
-        with open(self.SAVE_PATH, "a", newline="", encoding="utf-8") as csvfile:
+        """Saves the simulation results to a CSV file"""
+        file_exists = os.path.exists(self.SIMULATIONS_SAVE_PATH)
+        with open(self.SIMULATIONS_SAVE_PATH, "a", newline="", encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
 
             if not file_exists:
@@ -47,7 +56,7 @@ class BirthdaySim:
                 ])
                 row_id = 1
             else:
-                with open(self.SAVE_PATH, "r", encoding="utf-8") as f:
+                with open(self.SIMULATIONS_SAVE_PATH, "r", encoding="utf-8") as f:
                     lines = f.readlines()
                     if len(lines) > 1:
                         last_id = int(lines[-1].split(',')[0])
@@ -65,10 +74,45 @@ class BirthdaySim:
 
     @staticmethod
     def _format_num(num: int) -> str:
-        """Formats number to string"""
+        """Formats a number with spaces for readability"""
         return f"{num:_}".replace("_", " ")
+
+    def draw_graph(self, save: bool = False) -> None:
+        """Draws a line graph of previous simulation results"""
+        group_size = []
+        probability = []
+
+        try:
+            with open(self.SIMULATIONS_SAVE_PATH, "r", encoding="utf-8") as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    group_size.append(int(row["group_size"]))
+                    probability.append(float(row["probability"]))
+        except FileNotFoundError:
+            print("[ERROR] File doesn't exist")
+            return
+
+        fig, ax = plt.subplots()
+
+        ax.plot(sorted(group_size), sorted(probability), marker="o")
+
+        ax.set_title("Birthday Paradox")
+        ax.set_xlabel("Group size")
+        ax.set_ylabel("Probability of a match (%)")
+
+        ax.grid(True)
+
+        ax.set_xticks(group_size)
+        ax.set_yticks(range(0, 101, 10))
+
+        if save:
+            print(f"Plot saved to {self.PLOT_SAVE_PATH}")
+            fig.savefig(self.PLOT_SAVE_PATH)
+        else:
+            plt.show()
 
 
 if __name__ == "__main__":
     birthday_sim = BirthdaySim()
     birthday_sim.simulate()
+    birthday_sim.draw_graph()
